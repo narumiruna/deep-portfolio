6814bdaea3d3b66d9ee843288c4ad1e86c0a7b3f
KuiMing <benjamin0901@gmail.com>
2022-05-31 08:32:23 +0800

    numpy split

--- scripts/data_split.py
+++ scripts/data_split.py
@@ -19,7 +19,4 @@ def data_split(data, data_len=50, val_ratio=0.2, test_ratio=0.1):
     x_all = np.array(x_all)
     train_end = int(len(x_all) * (1 - (val_ratio + test_ratio)))
     val_end = int(len(x_all) * (1 - test_ratio))
-    x_train = x_all[:train_end]
-    x_val = x_all[train_end:val_end]
-    x_test = x_all[val_end:]
-    return x_train, x_val, x_test
+    return np.split(x_all, [train_end, val_end])
