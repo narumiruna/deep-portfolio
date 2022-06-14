09a476c98fd65880b6b5e293364ec5814d9292dd
なるみ <narumi@maicoin.com>
2022-06-15 01:51:18 +0800

    add from_config method to initialize trainer

--- deepportfolio/cli.py
+++ deepportfolio/cli.py
@@ -1,6 +1,5 @@
 import click
 import mlconfig
-import torch
 
 
 @click.group()
@@ -12,29 +11,7 @@ def cli():
 @click.option('-c', '--config-file', default='configs/default.yaml')
 def train(config_file):
     cfg = mlconfig.load(config_file)
-
-    device = torch.device(cfg.device)
-
-    model = cfg.model()
-    model.to(device)
-
-    loss_fn = cfg.loss_fn()
-
-    optimizer = cfg.optimizer(model.parameters())
-    scheduler = cfg.scheduler(optimizer)
-
-    train_loader = cfg.train_loader()
-    valid_loader = cfg.valid_loader()
-
-    trainer = cfg.trainer(
-        device=device,
-        model=model,
-        loss_fn=loss_fn,
-        optimizer=optimizer,
-        scheduler=scheduler,
-        train_loader=train_loader,
-        valid_loader=valid_loader,
-    )
+    trainer = cfg.trainer(cfg, classmethod='from_config')
     trainer.fit()
 
 
