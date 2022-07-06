9b982bfa97276c89c31a925502c900e009fbcfd0
なるみ <narumi@maicoin.com>
2022-07-07 02:52:05 +0800

    support short-selling and leverage

--- deepportfolio/nn/portfolio_net.py
+++ deepportfolio/nn/portfolio_net.py
@@ -12,10 +12,18 @@ class PortfolioNet(nn.Sequential):
                  output_dim: int,
                  hidden_dim: int = 64,
                  num_layers: int = 1,
-                 rnn_type: str = 'LSTM'):
+                 rnn_type: str = 'LSTM',
+                 short_selling: bool = False,
+                 leverage: float = 1.0):
         super(PortfolioNet, self).__init__()
+        self.short_selling = short_selling
+        self.leverage = leverage
+
         if rnn_type not in ('RNN', 'GRU', 'LSTM'):
             raise ValueError('rnn_type should be RNN, GRU or LSTM')
+
+        if leverage <= 0:
+            raise ValueError('leverage should be greater than 0')
 
         self.rnn = getattr(nn, rnn_type)(input_size=input_dim,
                                          hidden_size=hidden_dim,
@@ -27,6 +35,11 @@ class PortfolioNet(nn.Sequential):
         out, _ = self.rnn(x)  # (batch_size, seq_len, hidden_dim)
         out = self.fc(out)  # (batch_size, seq_len, output_dim)
 
-        out = F.softmax(out, dim=2)
+        if self.short_selling:
+            out = out.sign() * F.softmax(out.abs(), dim=2)
+        else:
+            out = F.softmax(out, dim=2)
+
+        out.mul_(self.leverage)
 
         return out
